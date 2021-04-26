d2967480db45c196eb084b027b1caa10c952856b
emre9180 <79599681+emre9180@users.noreply.github.com>
2021-04-26 04:43:15 +0300

    Add files via upload

---
+++ 1901042606_HW1/1901042606/1901042606.ino
@@ -0,0 +1,21 @@
+void setup() {
+  
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+}
+
+
+void loop() {
+  
+   Serial.println("Hello, this is CSE101 Assignment-1");
+   Serial.println("Emre YILMAZ - 1901042606");
+
+   
+   while (true) {
+     continue;
+   }
+ 
+}
